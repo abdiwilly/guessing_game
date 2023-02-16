b530552392cda68188b7fbd431273766f727d739
Abdiwilly <abdiwillyhassan@gmail.com>
2023-02-16 09:25:52 +0300

    added hello message

---
+++ game.py
@@ -0,0 +1,8 @@
+# this program demonstrates a guessing game
+#get user input
+#generate random number
+#using a while loop
+#check if user input is equal o generated number
+
+username=input("Enter your name:")
+print("Hello"+ " " + username)
